f48e184712e1a9d9d8446a34c915ef9c1050aff6
justin-tanvt <justintan28@gmail.com>
2025-04-06 23:14:46 +0800

    [AUTO]: checked in from macbook-air

--- src/LearnLinearSearch.java
+++ src/LearnLinearSearch.java
@@ -2,14 +2,22 @@ import java.util.Arrays;
 
 public class LearnLinearSearch {
 	public static void main(String[] args) {
-		char[] alphabets = new char[26];
 		int UPPERCASE_ALPHABETS_ASCII_OFFSET = 65;
+		int NUMBER_OF_CHARS = 26;
 
-		for (int i = 0; i < 26 * 2; i++) {
+		char[] alphabets = new char[NUMBER_OF_CHARS];
+		for (int i = 0; i < NUMBER_OF_CHARS; i++) {
 			alphabets[i] = (char) (i + UPPERCASE_ALPHABETS_ASCII_OFFSET);
 		}
 
 		System.out.println(Arrays.toString(alphabets));
+
+		int index = linearSearch(alphabets, 'Z');
+		if (index != -1) {
+			System.out.println("element found at index: " + index);
+		} else {
+			System.out.println("element not found");
+		}
 	}	
 
 	private static int linearSearch(char[] array, char value) {
@@ -18,7 +26,6 @@ public class LearnLinearSearch {
 				return i;
 			}
 		}	
-
 		return -1;
 	}
 }
