0f73cf729996b9d422a413a0ec779a986a792f0b
justin-tanvt <justintan28@gmail.com>
2025-04-06 23:11:08 +0800

    [AUTO]: checked in from macbook-air

--- src/LearnLinearSearch.java
+++ src/LearnLinearSearch.java
@@ -5,10 +5,20 @@ public class LearnLinearSearch {
 		char[] alphabets = new char[26];
 		int UPPERCASE_ALPHABETS_ASCII_OFFSET = 65;
 
-		for (int i = 0; i < 26; i++) {
+		for (int i = 0; i < 26 * 2; i++) {
 			alphabets[i] = (char) (i + UPPERCASE_ALPHABETS_ASCII_OFFSET);
 		}
 
 		System.out.println(Arrays.toString(alphabets));
 	}	
+
+	private static int linearSearch(char[] array, char value) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return i;
+			}
+		}	
+
+		return -1;
+	}
 }
