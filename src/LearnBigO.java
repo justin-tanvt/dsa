f5049ea7ef820f54a5e05ecf29d48ecc07f4dc71
justin-tanvt <justintan28@gmail.com>
2025-04-06 22:50:49 +0800

    [AUTO]: checked in from macbook-air

---
+++ src/LearnBigO.java
@@ -0,0 +1,24 @@
+public class LearnBigO {
+	public static void main(String[] args) {
+		/*
+		O(1) 				= constant time
+		O(log n) 		= logarithmic time
+		O(n) 				= linear time
+		O(n log n) 	= quasilinear time	
+		O(n^2) 			= quadratic time
+		O(n!)				= factorial time
+
+		EXCEPTIONS TO BIG O (applicable for small data sets)
+		> hardware perspective
+			- spacial and temporal locality of data access (CPU pre-loads into caches)
+			- CPU performs branch prediction (which favours simpler logic)
+			- SIMD optimisations favour contiguous data and uniform/predictable operations
+		> software perspective
+			- think about overhead of algorithm (function call, setup cost, memory allocation)
+				i.e. different sorts OR recursion OR linear vs binary search
+			- think about memory access patterns (contiguous for spacial, uniformity and simplicity for temporal)
+			- think about in-place vs new memory allocation
+			- think about algorithmic complexity (branch prediction and SIMD optimisations)
+		*/	
+	}
+}
