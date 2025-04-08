45a764cf0fe22292a0532a96c50c954981ce7580
justin-tanvt <justintan28@gmail.com>
2025-04-08 21:54:01 +0800

    [AUTO]: checked in from macbook-air

---
+++ src/LearnRecursion.java
@@ -0,0 +1,33 @@
+public class LearnRecursion {
+	public static void main(String[] args) {
+		System.out.println(iFactorial(7));
+		System.out.println(rFactorial(7));
+		System.out.println(power(2, 8));
+	}
+
+	private static int iFactorial(int num) {
+		int ans = 1;
+		for (int i = 1; i <= num; i++) {
+			ans *= i;
+		}
+		return ans;
+	}
+
+	private static int rFactorial(int num) {
+		// base case
+		if (num == 1) {
+			return 1;
+		}
+
+		return num * rFactorial(num - 1);
+	}
+
+	private static int power(int base, int exponent) {
+		// base case
+		if (exponent < 1) {
+			return 1;
+		}
+
+		return base * power(base, exponent - 1);
+	}
+}
