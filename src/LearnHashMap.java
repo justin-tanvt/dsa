ba8a31b3f70290c0da944c67c919c3b91d97693b
justin-tanvt <justintan28@gmail.com>
2025-04-10 09:01:41 +0800

    [AUTO]: checked in from macbook-air

---
+++ src/LearnHashMap.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class LearnHashMap {
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<>(10);	
+
+		map.put(100, "Spongebob");
+  	map.put(123, "Patrick");
+  	map.put(321, "Sandy");
+  	map.put(555, "Squidward");
+  	map.put(777, "Gary");
+
+  	for (Integer key : map.keySet()) {
+  		System.out.println(key.hashCode() % 10 + "\t" + key + "\t" + map.get(key));
+  	}
+	}
+}
