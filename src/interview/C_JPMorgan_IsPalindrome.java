92ddc8aa9e353e63369918679cca25d13d53e165
justin-tanvt <justintan28@gmail.com>
2025-04-12 10:36:07 +0800

    [MANUAL]: added JPM round1

---
+++ src/interview/C_JPMorgan_IsPalindrome.java
@@ -0,0 +1,32 @@
+package interview;
+
+public class C_JPMorgan_IsPalindrome {
+  public static void main(String[] args) {
+    String yes = "ABA";
+    String no = "APPLE";
+
+    System.out.println(isPalindrome(yes));
+    System.out.println(isPalindrome(no));
+  }
+
+  private static boolean isPalindrome(String str) {
+    // get string length
+    int length = str.length();
+    // divide string length by half = k
+    int k = length / 2;     // 5 / 2 = 2
+
+    // for loop through k
+    for (int i = 0; i <= k; i++) {
+      // get char from left
+      char l = str.charAt(i);
+      // get char from right
+      char r = str.charAt(length - i - 1);
+      // if not equal return false
+      if (l != r) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
