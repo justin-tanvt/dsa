7101f39f2da29777d6444007cc41c102d889725c
justin-tanvt <justintan28@gmail.com>
2025-04-13 14:29:24 +0800

    [AUTO]: checked in from macbook-air

---
+++ src/interview/D_Goldman_PrimeFactorisation.java
@@ -0,0 +1,34 @@
+package interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class D_Goldman_PrimeFactorisation {
+  public static void main(String[] args) {
+    System.out.println(primeFactorisation(5));
+    System.out.println(primeFactorisation(7));
+    System.out.println(primeFactorisation(12));
+    System.out.println(primeFactorisation(15));
+    System.out.println(primeFactorisation(124948012));
+  }
+
+  private static List<Integer> primeFactorisation(int n) {
+    List<Integer> factors = new ArrayList<>();
+
+    for (int f = 2; f <= n; f++) {
+      // while number is divisible by factor
+      while (n % f == 0) {
+        // divide n by f
+        n /= f;
+        // add f to factors
+        factors.add(f);
+      }
+
+      // number no longer divisible by factor
+      // move onto next factor
+    }
+
+    return factors;
+  }
+
+}
